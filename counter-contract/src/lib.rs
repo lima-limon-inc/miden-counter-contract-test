93c7183def705ebcc61ede209037dbc5afd65e52
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-12-03 18:55:05 -0300

    feat: add examples on the usage of Package in #[miden_test]
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- counter-contract/src/lib.rs
+++ counter-contract/src/lib.rs
@@ -43,12 +43,36 @@ mod component {
 mod tests {
     use winter_utils::Deserializable;
 
+    // This tests loads the generated package in the `foo` variable and is then
+    // printed in line 50.
     #[miden_test]
-    fn bar() {
-        std::dbg!(&package);
+    fn bar(foo: Package) {
+        std::dbg!(&foo);
         assert_eq!(1, 1 + 1);
     }
 
+    // This test will fail at compile time because it is only legal to have a
+    // single package as an argument. The following error message is displayed:
+    //
+    // error: custom attribute panicked
+    //   --> src/lib.rs:55:5
+    //    |
+    // 55 |     #[miden_test]
+    //    |     ^^^^^^^^^^^^^
+    //    |
+    //    = help: message:
+    //            Detected that all of the following variables are `Package`s: foo, bar
+    //
+    //            #[miden_test] only supports having a single `Package` in its argument list.
+    #[miden_test]
+    fn bing(foo: Package, bar: Package) {
+        std::dbg!(&foo);
+        assert_eq!(1, 1 + 1);
+    }
+
+    // This tests will not load the package since there is no argument declared
+
+    // with the Package type. This test will simply behave as normal cargo test.
     #[miden_test]
     fn foo() {
         assert_eq!(2, 1 + 1)
